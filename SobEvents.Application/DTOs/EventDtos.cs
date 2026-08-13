c199846cb083979cfc17463764c544fe8c92bf26
zfirstalpha <salim14sadik@gmail.com>
2026-08-13 17:00:09 +0300

    feat(api): add data annotations validation to CreateEventRequest

--- SobEvents.Application/DTOs/EventDtos.cs
+++ SobEvents.Application/DTOs/EventDtos.cs
@@ -1,10 +1,17 @@
+using System.ComponentModel.DataAnnotations;
 namespace SobEvents.Application.DTOs;
 
 //this is what the client will send us when creating event
 public record CreateEventRequest(
-    string Name, 
+   [Required (ErrorMessage = "Event name is required")]
+   [MaxLength(100, ErrorMessage = "Event name cannot exceed 100 characters")]
+   string Name, 
+    [MaxLength(500, ErrorMessage = "Event description cannot exceed 500 characters")]
     string Description, 
+    [Required(ErrorMessage = "Event date is required")]
     DateTime Date, 
+    [Required(ErrorMessage = "Event location is required")]
+    [MaxLength(200, ErrorMessage = "Event location cannot exceed 200 characters")]
     string Location
 );
 
